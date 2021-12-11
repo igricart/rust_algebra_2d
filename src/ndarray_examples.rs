3806932b736eb912b6b87912b48d16b8444a2e44
Ignacio Ricart <ignacio@poli.ufrj.br>
2021-12-11 18:50:32 +0100

    move example functions to another file

---
+++ src/ndarray_examples.rs
@@ -0,0 +1,53 @@
+use nalgebra::Matrix3;
+use ndarray::{arr1, arr2, Array1};
+
+pub fn example1() {
+  let a = arr2(&[[1, 2, 3], [4, 5, 6]]);
+
+  let b = arr2(&[[6, 5, 4], [3, 2, 1]]);
+
+  let sum = &a + &b;
+
+  println!("{}", a);
+  println!("+");
+  println!("{}", b);
+  println!("=");
+  println!("{}", sum);
+}
+
+pub fn example2() {
+  let a = arr2(&[[1, 2, 3], [4, 5, 6]]);
+
+  let b = arr2(&[[6, 3], [5, 2], [4, 1]]);
+
+  println!("{}", a.dot(&b));
+}
+
+pub fn example3() {
+  let scalar = 4;
+
+  let vector = arr1(&[1, 2, 3]);
+
+  let matrix = arr2(&[[4, 5, 6], [7, 8, 9]]);
+
+  let new_vector: Array1<_> = scalar * vector;
+  println!("{}", new_vector);
+
+  // Matrix multiplication is performed using dot
+  // while the * operator performs element-wise multiplication.
+  let new_matrix = matrix.dot(&new_vector);
+  println!("{}", new_matrix);
+}
+
+pub fn example4() {
+  let m1 = Matrix3::new(2.0, 1.0, 1.0, 3.0, 2.0, 1.0, 2.0, 1.0, 2.0);
+  println!("m1 = {}", m1);
+  match m1.try_inverse() {
+    Some(inv) => {
+      println!("The inverse of m1 is: {}", inv);
+    }
+    None => {
+      println!("m1 is not invertible!");
+    }
+  }
+}
