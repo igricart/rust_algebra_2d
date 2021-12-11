d66a50edb2afdc099208d55082acb25ac9725847
Ignacio Ricart <ignacio@poli.ufrj.br>
2021-12-11 12:08:14 +0100

    use ndarray as example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use ndarray::arr2;
+
 fn main() {
-    println!("Hello, world!");
+    let a = arr2(&[[1, 2, 3], [4, 5, 6]]);
+
+    let b = arr2(&[[6, 5, 4], [3, 2, 1]]);
+
+    let sum = &a + &b;
+
+    println!("{}", a);
+    println!("+");
+    println!("{}", b);
+    println!("=");
+    println!("{}", sum);
 }
